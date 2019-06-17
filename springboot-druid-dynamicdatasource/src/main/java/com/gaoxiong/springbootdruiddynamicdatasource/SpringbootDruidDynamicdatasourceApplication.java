c95327633e4cc001ff56ba9d503e771579e6e84f
200506725@qq.com <gaojunchen520>
2019-06-17 15:03:03 +0800

    1

---
+++ springboot-druid-dynamicdatasource/src/main/java/com/gaoxiong/springbootdruiddynamicdatasource/SpringbootDruidDynamicdatasourceApplication.java
@@ -0,0 +1,13 @@
+package com.gaoxiong.springbootdruiddynamicdatasource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDruidDynamicdatasourceApplication {
+
+    public static void main ( String[] args ) {
+        SpringApplication.run(SpringbootDruidDynamicdatasourceApplication.class, args);
+    }
+
+}
